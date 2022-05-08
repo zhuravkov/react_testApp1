85b56dea5294a1d61eb40e2454d88584430ef54a
sergey zhuravkov <zhuravkov.ss@yandex.ru>
2022-05-08 12:44:18 +0500

    sorting on start

--- src/features/posts/Posts.tsx
+++ src/features/posts/Posts.tsx
@@ -4,7 +4,7 @@ import {Paginator} from '../paginator/paginator';
 import { Search } from '../search/Search';
 import { Header } from './Header/Header';
 import { PostItem } from './PostItem/PostItem';
-import { getPostsThunk,selectPosts, selectCurrentPage, selectPageSize } from './postsSlice';
+import { getPostsThunk,selectPosts, selectCurrentPage, selectPageSize, PostType } from './postsSlice';
 import styles from './Posts.module.css';
 
 
@@ -18,10 +18,25 @@ export const Posts = React.memo(() => {
 
     let totalPostsCount = posts.length
 
+
+    // onClick sort
+    const newBaz = posts.slice()
+
+    let SortArray = (param:any) => (x:any, y:any)=>{
+    if (x[param] < y[param]) {return -1;}
+    if (x[param] > y[param]) {return 1;}
+    return 0;
+}
+     
+    //   newBaz.sort(compare);
+    console.log(newBaz.sort(SortArray('title')))
+
+
     // Do slices from origin posts
     let sliceStart = Math.ceil((currentPage - 1) * 100 / pageSize)
     let sliceEnd = currentPage * pageSize
-    let postsOnPage = posts.slice(sliceStart, sliceEnd)
+    
+    let postsOnPage = newBaz.slice(sliceStart, sliceEnd)
 
     // dispatch ThunkCreator getting posts async
     useEffect(() => {
